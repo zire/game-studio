2369eda0d1b72c900c658668d525f753e1718e43
Herbert Yang <herbert.yang@gmail.com>
2025-07-05 14:45:23 +0800

    Add progress web application implementation

---
+++ src/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pacman-v1';
+const urlsToCache = [
+  '/',
+  '/pacman.html',
+  '/waka.wav',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+}); 
